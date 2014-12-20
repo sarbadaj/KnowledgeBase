27557d211366aa622cc2cf8321dbbe563bdb1f44
sarbadaj <sarbadaj@yahoo.com>
2014-12-21 02:22:36 +0545

    created route for create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :knowledges, only: [:index, :new]
+  resources :knowledges, only: [:index, :new, :create]
   root 'knowledges#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

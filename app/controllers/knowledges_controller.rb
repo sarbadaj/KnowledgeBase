29b78ba0b70fac4dc578678c52f8982968586f1a
sarbadaj <sarbadaj@yahoo.com>
2014-12-21 02:19:04 +0545

    implemented featue of create new knowledge on knowledge base

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,4 +1,20 @@
 class KnowledgesController < ApplicationController
-  def index
+  def new
+    @knowledge = Knowledge.new
   end
+
+  def index
+    @knowledges = Knowledge.all
+  end
+
+  def create
+    @knowledge = Knowledge.new(knowledge_params)
+    @knowledge.save
+    redirect_to knowledges_path
+  end
+
+  private
+    def knowledge_params
+      params.require(:knowledge).permit(:title, :body)
+    end
 end

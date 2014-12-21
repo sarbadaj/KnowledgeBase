e0533cdce275956ffbce6d751b16f650efadeddf
sarbadaj <sarbadaj@yahoo.com>
2014-12-21 15:54:15 +0545

    completed test for create new knowledge

--- spec/feature/create_knowledge_spec.rb
+++ spec/feature/create_knowledge_spec.rb
@@ -3,6 +3,9 @@ feature "creating a new knowledge base document", :js => true do
   scenario "create new knowledge" do
     visit '/'
     click_button 'Knowledge'
-    binding.pry
+    fill_in 'Title', :with => "git details"
+    fill_in 'Body', :with => "Details knowledge of git."
+    click_button 'Save'
+    visit '/knowledges'
   end 
 end
